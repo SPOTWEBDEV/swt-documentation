f95e1c8ec63043202fb1c7828e49f95533af64ad
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-09-03 17:36:08 +0100

    added a hot reload

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,9 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
